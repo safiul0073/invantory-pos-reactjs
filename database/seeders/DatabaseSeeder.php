5c68f4ff2e8e381ea24b3f9bcaee488fc29bf15b
Anis5221 <parsonal494@gmail.com>
2021-10-27 18:20:38 +0600

    project to git

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        \App\Models\User::create([
+            'first_name' => "Admin",
+            'last_name' => "Admin",
+            'email' => "admin@mail.com",
+            'email_verified_at' => now(),
+            'password' => bcrypt('admin'),
+
+            'remember_token' => Str::random(10),
+        ]);
+        $this->call(SettingsSeeder::class);
+    }
+}
